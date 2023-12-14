00f7670929776f1367c62246ebf51c7039c11e36
shecanfixme <gorocsmartonxd@gmail.com>
2023-12-14 12:55:59 +0100

    asd

--- example.py
+++ example.py
@@ -1,3 +1,33 @@
-import random
+import os
+import random as rnd
+os.system('cls')
 
-print("Számok gyakorisága egy adott intervallumban")
+def isitPerfect():
+    pass
+
+def randomGenerator(s, e, a):
+    numbers = list()
+    for i in range(a):
+        numbers.append(random.randint(s, e))
+    return numbers
+        
+def makeNumber(text):
+    isCorrect = False
+    while True:
+        n = input(text)
+        try:
+            n = int(n)
+            #isCorrect = True
+            return n
+        except ValueError:
+            print("Helytelen érték.")
+    
+startMessage = "Kezdő érték: "
+endMessage = "Végérték: "
+amountMessage = "Értékek száma: "
+
+start = makeNumber(startMessage)
+end = makeNumber(endMessage)
+amount = makeNumber(amountMessage)
+
+randomGenerator(start, end, amount)
